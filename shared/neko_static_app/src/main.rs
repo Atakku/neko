e331843fdbc167fe3f65a495a12d936816240c53
Atakku <atakkudev@gmail.com>
2022-12-27 20:17:25 +0100

    Fix trunk not working

--- shared/neko_static_app/src/main.rs
+++ shared/neko_static_app/src/main.rs
@@ -11,16 +11,11 @@ fn handle_embedded_file(path: &str) -> HttpResponse {
     Some(content) => HttpResponse::Ok()
       .content_type(from_path(path).first_or_octet_stream().as_ref())
       .body(content.data.into_owned()),
-    None => HttpResponse::NotFound().finish(),
+    None => handle_embedded_file("index.html"),
   }
 }
 
-#[actix_web::get("/")]
-async fn index() -> impl Responder {
-  handle_embedded_file("index.html")
-}
-
-#[actix_web::get("/{_:.*}")]
+#[actix_web::get("{_:.*}")]
 async fn dist(path: web::Path<String>) -> impl Responder {
   handle_embedded_file(path.as_str())
 }
@@ -28,7 +23,7 @@ async fn dist(path: web::Path<String>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   let port = neko_env::HTTP_PORT.to_owned();
-  HttpServer::new(|| App::new().service(index).service(dist))
+  HttpServer::new(|| App::new().service(dist))
     .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
