93ce7af32dd829292cc5d8f90e7c82cab1bff3b4
Atakku <atakkudev@gmail.com>
2023-09-25 06:01:37 +0200

    forgor to commit

--- src/modules/ftv.rs
+++ src/modules/ftv.rs
@@ -2,14 +2,12 @@
 //
 // This project is dual licensed under MIT and Apache.
 
-use super::poise::{EventHandler, Ctx};
 use crate::{core::*, modules::poise::Poise};
 use poise::{
-  serenity_prelude::{ChannelId, Colour, GuildId, User},
-  Event, BoxFuture,
+  serenity_prelude::{ChannelId, Colour, GuildId, User}, BoxFuture, Event,
 };
 
-const GUILD: GuildId = GuildId(1038789193113014333);
+pub const GUILD: GuildId = GuildId(1038789193113014333);
 const GENERAL: ChannelId = ChannelId(1038997543515865148);
 
 /// Module with femboy.tv discord server functionality
@@ -24,53 +22,53 @@ impl Module for FemboyTV {
 }
 
 fn welcomer<'a>(c: &'a poise::serenity_prelude::Context, event: &'a Event<'a>) -> BoxFuture<'a, R> {
-    Box::pin(async move {
-      use Event::*;
-      match event {
-        GuildMemberAddition { new_member: m } => {
-          if !m.user.bot && m.guild_id == GUILD {
-            let u = &m.user;
-            GENERAL
-              .send_message(c, |m| {
-                m.embed(|e| {
-                  e.author(|a| {
-                    a.icon_url(get_avatar(&u));
-                    a.name(get_name(&u));
-                    a.url(format!("https://discord.com/users/{}", u.id))
-                  });
-                  e.colour(Colour::from_rgb(139, 195, 74));
-                  e.description(format!("Welcome <@{}> to the server!", u.id))
-                })
+  Box::pin(async move {
+    use Event::*;
+    match event {
+      GuildMemberAddition { new_member: m } => {
+        if !m.user.bot && m.guild_id == GUILD {
+          let u = &m.user;
+          GENERAL
+            .send_message(c, |m| {
+              m.embed(|e| {
+                e.author(|a| {
+                  a.icon_url(get_avatar(&u));
+                  a.name(get_name(&u));
+                  a.url(format!("https://discord.com/users/{}", u.id))
+                });
+                e.colour(Colour::from_rgb(139, 195, 74));
+                e.description(format!("Welcome <@{}> to the server!", u.id))
               })
-              .await?;
-          }
+            })
+            .await?;
         }
-
-        GuildMemberRemoval {
-          guild_id: g,
-          user: u,
-          member_data_if_available: _,
-        } => {
-          if !u.bot && *g == GUILD {
-            GENERAL
-              .send_message(c, |m| {
-                m.embed(|e| {
-                  e.author(|a| {
-                    a.icon_url(get_avatar(&u));
-                    a.name(get_name(&u));
-                    a.url(format!("https://discord.com/users/{}", u.id))
-                  });
-                  e.colour(Colour::from_rgb(244, 67, 54));
-                  e.description(format!("<@{}> has left the server!", u.id))
-                })
-              })
-              .await?;
-          }
-        }
-        _ => {}
       }
-      Ok(())
-    })
+
+      GuildMemberRemoval {
+        guild_id: g,
+        user: u,
+        member_data_if_available: _,
+      } => {
+        if !u.bot && *g == GUILD {
+          GENERAL
+            .send_message(c, |m| {
+              m.embed(|e| {
+                e.author(|a| {
+                  a.icon_url(get_avatar(&u));
+                  a.name(get_name(&u));
+                  a.url(format!("https://discord.com/users/{}", u.id))
+                });
+                e.colour(Colour::from_rgb(244, 67, 54));
+                e.description(format!("<@{}> has left the server!", u.id))
+              })
+            })
+            .await?;
+        }
+      }
+      _ => {}
+    }
+    Ok(())
+  })
 }
 
 fn get_avatar(u: &User) -> String {
