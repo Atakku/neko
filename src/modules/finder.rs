b09e311c16e6a3f11da28c9b28d675911a82f21f
Atakku <atakkudev@gmail.com>
2024-06-09 06:47:12 +0200

    Restrict to ftv

--- src/modules/finder.rs
+++ src/modules/finder.rs
@@ -10,6 +10,7 @@ use crate::{
 };
 use geoutils::Location;
 use itertools::Itertools;
+use poise::serenity_prelude::GuildId;
 
 /// Module with femboy.tv discord server functionality
 pub struct Finder;
@@ -29,8 +30,16 @@ cmd_group!(finder, "find", "set");
 
 #[poise::command(slash_command)]
 pub async fn find(ctx: Ctx<'_>) -> R {
+  if ctx.guild_id() != Some(GuildId(1232659990993702943)) {
+    ctx
+      .send(|m| m.ephemeral(true).reply(true).content("This command is not available outside femboy.tv!"))
+      .await?;
+    return Ok(());
+  }
+
   let mut data = get_all().await?;
-  let Some((pos, (_, lat, lng, _name))) = data.clone()
+  let Some((pos, (_, lat, lng, _name))) = data
+    .clone()
     .into_iter()
     .find_position(|(name, _lat, _lng, _name)| *name == ctx.author().name)
   else {
@@ -51,13 +60,11 @@ pub async fn find(ctx: Ctx<'_>) -> R {
     .map(|(id, lat, lng, name)| {
       origin
         .distance_to(&Location::new(lat, lng))
-        .and_then(|dist| Ok((id, (dist.meters()/1000.0) as i64, name)))
+        .and_then(|dist| Ok((id, (dist.meters() / 1000.0) as i64, name)))
     })
     .filter_map(Result::ok)
-    .sorted_by_key(|(_, dist, _)| *dist).collect::<Vec<_>>();
-
-
-  
+    .sorted_by_key(|(_, dist, _)| *dist)
+    .collect::<Vec<_>>();
 
   let mut output = String::new();
   for (id, dist, name) in data.iter().take(25) {
@@ -72,6 +79,12 @@ pub async fn find(ctx: Ctx<'_>) -> R {
 
 #[poise::command(slash_command)]
 pub async fn set(ctx: Ctx<'_>, #[autocomplete = "finder_cities"] city_id: String) -> R {
+  if ctx.guild_id() != Some(GuildId(1232659990993702943)) {
+    ctx
+      .send(|m| m.ephemeral(true).reply(true).content("This command is not available outside femboy.tv!"))
+      .await?;
+    return Ok(());
+  }
   update_city(ctx.author().id.0 as i64, city_id.parse().unwrap()).await?;
   ctx
     .send(|m| m.ephemeral(true).reply(true).content("Updated your city!"))
