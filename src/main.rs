e302059018a1ac42f07a71cc6e2278b58e433c7e
Atakku <atakkudev@gmail.com>
2024-06-09 06:07:46 +0200

    Forgor to init

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ async fn main() -> R {
     fw.init_module(welcomer::Welcomer).await?;
     fw.init_module(ftvroles::FTVRoles).await?;
     fw.init_module(starboard::Starboard).await?;
+    fw.init_module(finder::Finder).await?;
   //}
   fw.init_module(streak::PostStreak).await?;
   fw.run().await?;
