5f500375921c0507665a3d8c84642cac566f01d0
Atakku <atakkudev@gmail.com>
2024-08-23 12:39:52 +0200

    findr has been migrated to ftv

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> R {
     fw.init_module(welcomer::Welcomer).await?;
     fw.init_module(ftvroles::FTVRoles).await?;
     fw.init_module(starboard::Starboard).await?;
-    fw.init_module(findr::Findr).await?;
+    //fw.init_module(findr::Findr).await?;
     fw.init_module(streak::PostStreak).await?;
   fw.run().await?;
   Ok(())
