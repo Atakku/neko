95ae2b25956b4ba2f65c7685dd96d41f813426f6
Atakku <atakkudev@gmail.com>
2025-01-23 15:25:00 +0100

    buhhh

--- src/plugins/gwaaa/plugin.rs
+++ src/plugins/gwaaa/plugin.rs
@@ -316,6 +316,7 @@ async fn callback_minecraft(
 
   let res = req()
     .post("https://mc-auth.com/oAuth2/token")
+    .header("Accept", "application/json")
     .json(form_str)
     .send()
     .await.unwrap();
